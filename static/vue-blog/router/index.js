bc7ff38ccba7b67ce593b0ba2919f2173eaa1621
chenxing <chenxing@bookan.com.cn>
2020-08-28 16:09:03 +0800

    1

--- static/vue-blog/router/index.js
+++ static/vue-blog/router/index.js
@@ -1,17 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-
 Vue.use(Router)
 
 export default new Router({
 	routes: [
 		{ path: '/list/:page', component: () => import('@/components/ListView.vue')},
 		{ path: '/static/:slug+', component: () => import('@/components/StaticView.vue')},
-		{ path: '/think/:slug+', component: () => import('@/components/StaticView.vue')},
-		{ path: '/read/:slug+', component: () => import('@/components/StaticView.vue')},
-		{ path: '/watch/:slug+', component: () => import('@/components/StaticView.vue')},
-		{ path: '/listen/:slug+', component: () => import('@/components/StaticView.vue')},
+		{ path: '/post/:slug+', component: () => import('@/components/StaticView.vue')},
 		{ path: '*', redirect: '/list/1' }
 	]
 });
