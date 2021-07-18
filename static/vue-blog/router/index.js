684d1f3d1102a028b27451303d580574235e76fb
chenxing <chenxing@bookan.com.cn>
2021-07-18 17:04:37 +0800

    toc

--- static/vue-blog/router/index.js
+++ static/vue-blog/router/index.js
@@ -4,6 +4,13 @@ const routes = [
 	{ path: '/list/:page', component: () => import('../components/ListView.vue') },
 	{ path: '/static/:slug+', component: () => import('../components/StaticView.vue') },
 	{ path: '/post/:slug+', component: () => import('../components/StaticView.vue') },
+	{
+		path: '/:catchAll(toc-.*)', beforeEnter: (to, from) => {
+			let anchorElement = document.getElementById(to.params.catchAll);
+			if (anchorElement) anchorElement.scrollIntoView();
+			return false
+		},
+	},
 	{ path: '/:catchAll(.*)', redirect: '/list/1' }
 ]
 
